2d7e6fb7b68854a36c46074702c1d7cb6f8327cb
amy <you@example.com>
2021-07-12 20:18:06 +1200

    Modified Listeners.java

--- src/test/java/customlisteners/Listeners.java
+++ src/test/java/customlisteners/Listeners.java
@@ -9,50 +9,47 @@ public class Listeners implements ITestListener {
 
 	public void onTestStart(ITestResult result) {
 		// TODO Auto-generated method stub
-	
+
 	}
 
 	public void onTestSuccess(ITestResult result) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Test passed is : "+result.getName());
-	
+
+		System.out.println("Test passed is : " + result.getName());
+
 	}
 
 	public void onTestFailure(ITestResult result) {
 		// TODO Auto-generated method stub
-		
-		
+
 		Reporter.log("Screen shot link");
-		System.out.println("capture screenshot for failed test: "+result.getName());
-		
+		System.out.println("capture screenshot for failed test: " + result.getName());
+
 	}
 
 	public void onTestSkipped(ITestResult result) {
 		// TODO Auto-generated method stub
-	
+
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onTestFailedWithTimeout(ITestResult result) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onStart(ITestContext context) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void onFinish(ITestContext context) {
 		// TODO Auto-generated method stub
 
 	}
-	
-	
 
 }
